2c0ec955549eaed7a146084aba972f8b9247481d
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-04-30 21:08:54 +1000

    Load Play & Pause Song

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const nextBtn = document.getElementById('next');
 
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress_container');
+const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+
+// Song Titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep Track Of Songs
+let songIndex = 2;
+
+// Initially Load Song Details Into DOM
+loadSong(songs(songIndex));
+
+// Update Song Details
+function loadSong(song) {
+  title.innerText = song;
+}
